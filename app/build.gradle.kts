3fb87b364c4bccda60b2c190827fe19b20b6ec76
PaulMPR <123373355+MPR2023@users.noreply.github.com>
2023-10-05 17:53:30 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,17 +5,18 @@ plugins {
 
 android {
     namespace = "com.example.cognisteear"
-    compileSdk = 30
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cognisteear"
         minSdk = 30
-        targetSdk = 30
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         vectorDrawables {
             useSupportLibrary = true
         }
+
     }
 
     buildTypes {
@@ -48,22 +49,19 @@ android {
 }
 
 dependencies {
-    // Downgrade dependencies here
-    // ...
-}
-dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("com.google.android.gms:play-services-wearable:17.1.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
     implementation("androidx.percentlayout:percentlayout:1.0.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.wear.compose:compose-material:1.2.0")
     implementation("androidx.wear.compose:compose-foundation:1.2.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
